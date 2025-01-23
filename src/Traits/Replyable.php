d91fcb1fb3bd940fdbd5d405c2518e6790c65e2b
Palaniappan P <pal@targetbay.com>
2025-01-23 14:41:12 +0530

    Fixed optional cc, bcc

--- src/Traits/Replyable.php
+++ src/Traits/Replyable.php
@@ -405,11 +405,18 @@ trait Replyable
 		$this->symfonyEmail
 			->from($this->fromAddress())
 			->to($this->toAddress())
-			->cc($this->returnCopies($this->cc))
-			->bcc($this->returnCopies($this->bcc))
+			// ->cc($this->returnCopies($this->cc))
+			// ->bcc($this->returnCopies($this->bcc))
 			->subject($this->subject)
 			->html($this->message)
 			->priority($this->priority);
+
+		if (!isset($this->cc)) {
+			$this->symfonyEmail->cc($this->returnCopies($this->cc));
+		}
+		if (!isset($this->bcc)) {
+			$this->symfonyEmail->bcc($this->returnCopies($this->bcc));
+		}
 
 		foreach ($this->attachments as $file) {
 			$this->symfonyEmail->attachFromPath($file);
